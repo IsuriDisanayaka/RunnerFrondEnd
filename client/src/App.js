a03a7ea404d08acb52f34f55c8bc44546f3098ea
IsuriDisanayaka <isuriumeshika1@gmail.com>
2023-05-04 16:25:37 +0530

    #3 Feat :Create home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import * as React from "react";
 import Runner from "./screen/Runner/index";
+import HomePage from './screen/homepage/index';
 
 function App() {
   return <Runner />;
